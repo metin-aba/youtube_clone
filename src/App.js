d5f5fe9d65fe4e35fa5e46209357c32d4d4cb297
Metinaba1905 <abametin722@gmail.com>
2023-06-26 14:56:32 +0300

    BrowserRouter ile router'lar eklendi.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import { BrowserRouter, Route, Router } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+      <Box sx={{backgroundColor:'#000'}}>
+        <Navbar/>
+        <Router>
+          <Route path='/' exact element={<Feed/>}/>
+          <Route path='/'  element={<VideoDetail/>}/>
+          <Route path='/'  element={<ChannelDetail/>}/>
+          <Route path='/'  element={<SearchFeed/>}/>
+        
+        </Router>
+      </Box>
+    </BrowserRouter>
   )
 }
 
