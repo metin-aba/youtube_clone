e9e4dcaaa9a6054d2425812d34bc5ab4deabdce5
Metinaba1905 <abametin722@gmail.com>
2023-06-26 23:29:48 +0300

    Videos component import edildi

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,6 +2,7 @@ import { Stack, Box ,Typography} from "@mui/material";
 
 import React from "react";
 import Sidebar from "./Sidebar";
+import Videos from "./Videos";
 
 const Feed = () => {
   return (
@@ -22,6 +23,14 @@ const Feed = () => {
         >
           Copyright 2023
         </Typography>
+      </Box>
+
+      <Box p={2} sx={{overflowY:'auto',height:'90vh' ,flex:2}}>
+        <Typography variant='h4' fontWeight='bold' mb={2} sx={{color:'white'}}>
+          New
+          <span style={{color:'#F31503'}}>videos</span>
+       </Typography>
+       <Videos videos={[]}/>
       </Box>
     </Stack>
   );
